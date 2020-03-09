3b6c0c31bc6e312813ec9f890a21eccc929d54cb
Azraf Chowdhury <achowdhur@ltu.edu>
2020-03-09 02:24:25 +0000

    updated routes and added helper methods' git push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :email)
+    params.require(:user).permit(:username, :password_digest, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/login' => 'session#new'
-  post '/login' => 'session#create'
-  post '/destroy' => 'session#destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/destroy' => 'sessions#destroy'
 
   resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
