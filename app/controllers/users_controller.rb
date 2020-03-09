b4f696c6d0cda426e8a905a611565cc5fd812859
Azraf Chowdhury <achowdhur@ltu.edu>
2020-03-09 02:42:25 +0000

    added password digest to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_digest, :email)
+    params.require(:user).permit(:username, :password, :email)
   end
 end
