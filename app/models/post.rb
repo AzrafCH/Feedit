9f4f3150f2673c501cb891d6bfd5b382ed53f03e
Azraf Chowdhury <achowdhur@ltu.edu>
2020-03-28 10:05:06 +0000

    stormy outside

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :subfed, optional: true
-  has_many :forums, dependent: :destroy
+  has_many :forums, dependent: :destroy, inverse_of: :posts
   has_many :subfeds, through: :forums, dependent: :destroy
   has_many :users, through: :forums, dependent: :destroy
 end
--- app/models/subfed.rb
+++ app/models/subfed.rb
@@ -1,6 +1,6 @@
 class Subfed < ApplicationRecord
-  belongs_to :user, optional: true
-  has_many :forums, dependent: :destroy
-  has_many :users, through: :forums, dependent: :destroy
-  has_many :posts, through: :forums, dependent: :destroy
+  belongs_to :user, optional: true, inverse_of: :subfed
+  has_many :forums, dependent: :destroy, inverse_of: :subfeds
+  has_many :users, through: :forums, dependent: :destroy, inverse_of: :subfeds
+  has_many :posts, through: :forums, dependent: :destroy, inverse_of: :subfeds
 end
