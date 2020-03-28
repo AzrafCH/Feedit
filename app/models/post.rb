0a3d9c521d3b45cbddeacff9f4d495bc2aac5f50
Azraf Chowdhury <achowdhur@ltu.edu>
2020-03-28 10:07:38 +0000

    loud thunder could lose power

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :subfed, optional: true
+  belongs_to :user, optional: true, inverse_of: :post
+  belongs_to :subfed, optional: true, inverse_of: :post
   has_many :forums, dependent: :destroy, inverse_of: :posts
-  has_many :subfeds, through: :forums, dependent: :destroy
-  has_many :users, through: :forums, dependent: :destroy
+  has_many :subfeds, through: :forums, dependent: :destroy, inverse_of: :posts
+  has_many :users, through: :forums, dependent: :destroy, inverse_of: :posts
 end
