4356bc0033ba2a33cd0d6fb33656e5db43820554
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-06 14:01:53 +0000

    SAVE

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :subfed
-  has_many :forums, foreign_key: :post_id, class_name: "Forum", dependent: :destroy
-  has_many :subfeds, through: :forums
-  has_many :users, through: :forums
+  belongs_to :user
 end
--- app/models/subfed.rb
+++ app/models/subfed.rb
@@ -1,6 +1,5 @@
 class Subfed < ApplicationRecord
 
-  has_many :forums, foreign_key: :subfed_id, class_name: "Forum", dependent: :destroy
-  has_many :users, through: :forums
-  has_many :posts, through: :forums
+  has_many :posts, foreign_key: :subfed_id, class_name: "Post", dependent: :destroy
+  has_many :users, through: :posts
 end
