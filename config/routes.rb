dba8c8a2f8c2ad375124d6ba247528f8f5141357
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-14 16:23:11 +0000

    make a seedfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :subfeds
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/destroy' => 'sessions#destroy'
