41b7f4b835b861f293314f502cbeae24dd1d7b06
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-15 21:05:25 +0000

    precompiled css templates

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( breakpoints.min.js )
+Rails.application.config.assets.precompile += %w( jquery.dropotron.min.js )
+Rails.application.config.assets.precompile += %w( util.js )
+Rails.application.config.assets.precompile += %w( main.js )
+Rails.application.config.assets.precompile += %w( browser.min.js )
+
 Rails.application.config.assets.precompile += %w( navbar.css )
+Rails.application.config.assets.precompile += %w( main.css )
