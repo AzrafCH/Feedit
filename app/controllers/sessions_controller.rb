a0decd3125a172a5c636a33b50c925c08ae9e07c
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-29 00:37:57 +0000

    changed email in login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to users_path(user.id)
     else
-      user = User.find_by(username: params[:user][:username])
+      user = User.find_by(email: params[:user][:email])
       if user && user.authenticate(params[:user][:password])
         session[:user_id] = user.id
         redirect_to users_path(user.id)
